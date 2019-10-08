65352ba651a8986be2024bd57cc54b3a460f2966
Jeremy <JeremyPlott@gmail.com>
2019-10-08 14:04:35 -0400

    Add project files.

---
+++ HRAppleAndOrange/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HRAppleAndOrange {
+    class Program {
+        static void Main(string[] args) {
+
+            int s = 7;
+            int t = 11;
+            int a = 5;
+            int b = 15;
+            int[] apples = { -2, 2, 1 };
+            int[] oranges = { 5, -6 };
+
+            int ac = 0;
+            int oc = 0;
+
+            foreach(var apple in apples) {
+                if(a + apple >= s && a + apple <= t) {
+                    ac++;
+                }
+            }
+            foreach(var orange in oranges) {
+                if(b + orange >= s && b + orange <= t) {
+                    oc++;
+                }
+            }
+            Console.WriteLine(ac);
+            Console.WriteLine(oc);
+        }
+    }
+}
